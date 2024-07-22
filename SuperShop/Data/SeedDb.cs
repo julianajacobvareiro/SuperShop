49ce2bb808dc58c36961381dfad6018ae1357f6e
julianajacobvareiro <jjacobareiro@gmail.com>
2024-07-22 13:00:24 +0100

    SeedDb

---
+++ SuperShop/Data/SeedDb.cs
@@ -0,0 +1,47 @@
+﻿using SuperShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SuperShop.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+
+            if(!_context.Products.Any())
+            {
+                AddProduct("Iphone X");
+                AddProduct("Magic Mouse");
+                AddProduct("iWatch Series 4");
+                AddProduct("Ipad Mini");
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private void AddProduct(string name)
+        {
+            _context.Products.Add(new Product
+            {
+                Name = name,
+                Price = _random.Next(1000),
+                IsAvailable = true,
+                StockPrice = _random.Next(100)
+            });
+        }
+    }
+}
