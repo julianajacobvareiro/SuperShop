aea979fdb99a0e1d8c3c0b772b9440025388ff78
julianajacobvareiro <jjacobareiro@gmail.com>
2024-10-07 12:59:50 +0100

    Reset e recover view model

--- SuperShop/Helpers/IUserHelper.cs
+++ SuperShop/Helpers/IUserHelper.cs
@@ -34,8 +34,10 @@ namespace SuperShop.Helpers
 
         Task<User> GetUserByIdAsync(string userId);
 
+        Task<string> GeneratePasswordResetTokenAsync(User user);
 
-        
+        Task<IdentityResult> ResetPasswordAsync(User user, string token, string password);
+
 
     }
 }
